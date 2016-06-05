638f7162664219b797a09ed90f703b831f88e33f
Weronika Pijagin <weronika.pijagin@gmail.com>
2016-06-05 18:39:07 +0200

    Add communication with a database where login and pwd are stored

---
+++ dbgenerator.py
@@ -0,0 +1,17 @@
+from sqlite3 import *
+db = connect('dbase.db')
+cur = db.cursor()
+
+table_name = 'dbase'
+first_col = 'login'
+second_col = 'password'
+typ = 'TEXT'
+
+
+db.execute('CREATE TABLE dbase (login TEXT, password TEXT)')
+
+db.execute("INSERT INTO dbase (login, password) VALUES ('aa153454', 'test')")
+db.execute("INSERT INTO dbase (login, password) VALUES ('120056', 'top secret')")
+
+db.commit()
+db.close()
--- safeyourself.py
+++ safeyourself.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from vial import Vial, render_template
+import sqlite3
+
+dbFile = '/home/pijaginw/safeyourself/safe-yourself/dbase.db'
 
 def index(headers, body, data):
   return 'Hello', 200, {}
@@ -11,8 +14,22 @@ def login(headers, body, data):
   request_method = headers['request-method']
   if request_method == 'GET':
     return render_template('login.html', body=body, data=data), 200, {}
+    
   elif request_method == 'POST':
+    login = data['inputLogin']
+    password = data['inputPass']
+
+    addToDatabase(login, password, dbFile)
     return 'login POST', 200, {}
+
+def addToDatabase(login, password, dbfile):
+  conn = sqlite3.connect(dbfile)
+  c = conn.cursor()
+
+  c.execute('INSERT INTO dbase VALUES (?, ?)', (str(login), str(password)))
+  conn.commit()
+  conn.close()
+
 
 def upload(headers, body, data):
   return render_template('upload.html', body=body, data=data), 200, {}
